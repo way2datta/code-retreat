5c0f906cc1cec779b1c8c548f15947fe60f5bf15
Dattatray Kale <way2datta@gmail.com>
2018-07-02 22:02:07 +0530

    Added classes to compute the final price

--- object-orientation/Program.cs
+++ object-orientation/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace object_orientation
 {
@@ -6,9 +7,26 @@ namespace object_orientation
     {
         internal static BillingDetails GetBillingDetails(Item scannedItem)
         {
+            decimal itemFinalPrice = CalculateFinalPrice(scannedItem);
+
             BillingDetails billingDetails = new BillingDetails(scannedItem);
 
+            billingDetails.FinalPrice = itemFinalPrice;
+
             return billingDetails;
+        }
+
+        private static decimal CalculateFinalPrice(Item item)
+        {
+            string category = ItemsCategoryMapping.GetCategoryFor(item.Name);
+
+            int gstRateForItem = GstRateProvider.GetRateFor(category);
+        
+            decimal gstRatePerItem = item.InitialPrice * gstRateForItem / 100;
+
+            decimal finalPrice = item.Quantity * (item.InitialPrice + gstRatePerItem);
+
+            return finalPrice;
         }
     }
 
@@ -23,6 +41,39 @@ namespace object_orientation
         public Item Item { get; set; }
     }
 
+    internal class GstRateProvider
+    {
+        private static Dictionary<string, int> CategoryGstRatesInPercentage = new Dictionary<string, int>();
+
+        static GstRateProvider()
+        {
+            SetupGstRateForCategories();
+        }
+
+        internal static int GetRateFor(string categoryName) 
+        {
+            int gstPercentageForItem = 0;
+
+            foreach (var categoryGstRate in CategoryGstRatesInPercentage)
+            {
+                if (categoryGstRate.Key == categoryName)
+                {
+                    gstPercentageForItem = categoryGstRate.Value;
+                    break;
+                }
+            }
+
+            return gstPercentageForItem;
+        }
+
+        private static void SetupGstRateForCategories()
+        {
+            CategoryGstRatesInPercentage.Add("Food-grains", 0);
+            CategoryGstRatesInPercentage.Add("Furniture", 5);
+            CategoryGstRatesInPercentage.Add("Electronics", 18);
+            CategoryGstRatesInPercentage.Add("Cosmetics", 28);
+        }
+    }
     internal class Item
     {
         public decimal InitialPrice { get; set; }
@@ -49,6 +100,63 @@ namespace object_orientation
         }
     }
 
+    internal class ItemsCategoryMapping
+    {
+        private static Dictionary<string, string> Mapping = new Dictionary<string, string>();
+
+        static ItemsCategoryMapping()
+        {
+            SetupItemsCategoryMapping();
+        }
+
+        internal static string GetCategoryFor(string itemName)
+        {
+            string categoryName = "";
+
+            foreach (var item in Mapping)
+            {
+                if (item.Key == itemName)
+                {
+                    categoryName = item.Value;
+                    break;
+                }
+            }
+
+            return categoryName;
+        }
+
+        private static void SetupItemsCategoryMapping()
+        {
+            SetupItemsForFoodGrains();
+            SetupItemsForFurniture();
+            SetupItemsForElectronics();
+            SetupItemsForCosmetics();
+        }
+
+        private static void SetupItemsForCosmetics()
+        {
+            Mapping.Add("Shampoo", "Cosmetics");
+            Mapping.Add("Perfume", "Cosmetics");
+        }
+
+        private static void SetupItemsForElectronics()
+        {
+            Mapping.Add("TV", "Electronics");
+            Mapping.Add("Mobile", "Electronics");
+        }
+
+        private static void SetupItemsForFoodGrains()
+        {
+            Mapping.Add("Rice", "Food-grains");
+            Mapping.Add("Wheat", "Food-grains");
+        }
+
+        private static void SetupItemsForFurniture()
+        {
+            Mapping.Add("Sofa", "Furniture");
+            Mapping.Add("Chairs", "Furniture");
+        }
+    }
     internal class NMartStore
     {
         private static void Main()
@@ -69,7 +177,7 @@ namespace object_orientation
         internal static void Print(BillingDetails billingDetails)
         {
             string output = "*******************************************\n" +
-                "Billing Details for " + billingDetails.Item.Name + ":\n" +
+                "Billing Details for '" + billingDetails.Item.Name + "':\n" +
                 "*******************************************\n" +
                 "Quantity: " + billingDetails.Item.Quantity +
                 "\nPrice per unit: " + billingDetails.Item.InitialPrice +
