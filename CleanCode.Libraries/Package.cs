38c4b8104344487966e1638374c1627c0143ab3d
Dattatray Kale <way2datta@gmail.com>
2018-10-14 16:25:09 +0530

    Added

---
+++ CleanCode.Libraries/Package.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCode.Libraries
+{
+    public class Package
+    {
+        public bool IsNotDamaged { get; set; }
+        public bool IsNotEmpty { get; set; }
+        public bool IsDamaged { get; set; }
+        public bool IsEmpty { get; set; }
+    }
+}
---
+++ CleanCodePractices/Program.cs
@@ -0,0 +1,56 @@
+﻿using CleanCode.Libraries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCodePractices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+
+        private static void AntiNegative() 
+        {
+            var package = FetchPackage();
+            if (!package.IsNotDamaged)
+            {
+                throw new PackageNotValidException("Package is damaged.");
+            }
+            if (!package.IsNotEmpty)
+            {
+                throw new PackageNotValidException("Package is empty.");
+            }
+        }
+
+        private static void Assertive()
+        {
+            var package = FetchPackage();
+            if (package.IsDamaged)
+            {
+                throw new PackageNotValidException("Package is damaged.");
+            }
+            if (package.IsEmpty)
+            {
+                throw new PackageNotValidException("Package is empty.");
+            }
+        }
+
+        public static Package FetchPackage()
+        {
+            return new Package();
+        }
+    }
+
+    public class PackageNotValidException : ApplicationException
+    {
+        public PackageNotValidException(string message) : base(message)
+        {
+        }
+    }
+}
